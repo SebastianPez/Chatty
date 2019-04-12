230c219d146e251f23de163cf1cf21de93e14dd7
SebastianPez <Sebastian.Pezarro@gmail.com>
2019-04-12 23:06:31 +0000

    Commented MesssageList.jsx

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -5,8 +5,10 @@ import SystemMessage from './SystemMessage.jsx';
 class MessageList extends Component {
     render() {
         const allMessages = this.props.messages.map(message => {
+            // Handling for user messages, assigning all necessary props to populate the Message elements.
             if (message.type === "postMessage") {
                 return <Message type={message.type} key={message.id} username={message.username} content={message.content} />
+            // Handling for server notifications, assigning all necessary props regardless of what message type it is.
             } else if (message.type === "postNotification") {
                 return <SystemMessage type={message.type} key={message.id} username={message.username} oldUserName={message.oldUserName} content={message.content} />
             }
