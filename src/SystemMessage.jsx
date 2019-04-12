e341198dce49f9abc020e8d405583157a3681804
SebastianPez <Sebastian.Pezarro@gmail.com>
2019-04-12 23:08:10 +0000

    Commented SystemMessage.jsx

--- src/SystemMessage.jsx
+++ src/SystemMessage.jsx
@@ -2,18 +2,21 @@ import React, {Component} from 'react';
 
 class SystemMessage extends Component {
     render() {
+        // If the notifcation is for a username update.
         if (this.props.oldUserName) {
             return (
                 <div className="message system">
                     {this.props.oldUserName} has changed their name to {this.props.username}.
                 </div>
             )
+        // If the notification is for a new user joining the channel.
         } else if (this.props.content === "New") {
             return (
                 <div className="message system">
                     A new user has joined the channel.
                 </div>
             )
+        // If the notification is for a user leaving the channel.
         } else if (this.props.content === "Lose") {
             return (
                 <div className="message system">
